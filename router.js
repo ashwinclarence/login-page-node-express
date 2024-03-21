9829b4d7f0825ed2a80aeeaff013f919380431a1
ASHWIN CLARENCE <meashwinclarence@gmail.com>
2024-03-21 21:13:49 +0530

    router error handling on login page added

--- router.js
+++ router.js
@@ -1,48 +1,38 @@
-var express=require('express');
-var router=express.Router();
+var express = require('express');
+var router = express.Router();
 
-const credential={
-
-    email:'meashwinclarence@gmail.com',
-    password:'12345'
+const credential = {
+    email: 'meashwinclarence@gmail.com',
+    password: '12345'
 }
 
 
 
-router.post('/login',(req,res)=>{
-
-    if(req.body.email===credential.email && req.body.password===credential.password){
-
-req.session.user=req.body.email;
-res.redirect('/route/dashboard');
-
-
-    }else{
-
-        res.end('invalid Username')
+router.post('/login', (req, res) => {
+    if (req.body.email === credential.email && req.body.password === credential.password) {
+        req.session.user = req.body.email;
+        res.redirect('/route/dashboard');
+    } else {
+        res.render('base',{errorMessage: "Invalid username or password"})
     }
 });
 
 
 
 
-router.get('/dashboard',(req,res)=>{
-
-    if(req.session.user){
-
-        res.render('dashboard',{user:req.session.user})
-    }else{
-
-        res.send('unauthorize user')
+router.get('/dashboard', (req, res) => {
+    if (req.session.user) {
+        res.render('dashboard', { user: req.session.user })
+    } else {
+        res.redirect('/')
     }
 })
 
 
 
-router.get('/logout',(req,res)=>{
-
+router.get('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/');
 })
 
-module.exports=router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,53 +1,53 @@
- 
- const express=require('express');
 
- const path = require('path');
+const express = require('express');
 
- const bodyparser=require('body-parser');
-const session=require('express-session');
- const {v4:uuidv4}=require("uuid");
- const nocache = require("nocache");
+const path = require('path');
 
- const router=require('./router');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const { v4: uuidv4 } = require("uuid");
+const nocache = require("nocache");
+
+const router = require('./router');
 
 
-const app=express();
+const app = express();
 
- const port=process.env.port||3000;
-app.use(bodyparser.json())
+const port = process.env.port || 3000;
+app.use(bodyParser.json())
 app.use(nocache());
-app.use(bodyparser.urlencoded({extended:true}))
- app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
+app.set('view engine', 'ejs');
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-app.use('/assets',express.static(path.join(__dirname,'public/assets')));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 app.use(session({
-    secret:uuidv4(),
-    resave:false,
-    saveUninitialized:true
-    
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: true
+
 }));
 
-app.use('/route',router);
+app.use('/route', router);
 
 
 
 
-app.get('/',(req,res)=>{
-    if(req.session.user){
-        res.redirect('/router/dashboard');
-    }else{
-        res.render('base',{titl:"login system"});
+app.get('/', (req, res) => {
+    if (req.session.user) {
+        res.redirect('/route/dashboard');
+    } else {
+        res.render('base', { title: "login system" });
     }
 
 
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
 
     console.log(`listening to the server on http://localhost:${port}`);
 })
